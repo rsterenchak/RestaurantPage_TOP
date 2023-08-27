e3930f42ea71ef1813f75f21a84bd82b25a738d4
rsterenchak <rsterenchak@gmail.com>
2023-08-27 11:09:02 +0000

    Added webpack --watch for live updates on server

--- restaurant_main/webpack.config.js
+++ restaurant_main/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+    mode: "development",
+    entry: {
+    index: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  mode: "development"
+    clean: true,
+  }
+  
 };
