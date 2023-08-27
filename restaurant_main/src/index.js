83fbc26d1a1d730a32985951db79962a6bb1470b
rsterenchak <rsterenchak@gmail.com>
2023-08-27 23:32:02 +0000

    Updated webpack build

--- restaurant_main/src/index.js
+++ restaurant_main/src/index.js
@@ -1,15 +1,25 @@
 import './style.css';
+import Image from './restaurant.jpg';
 
 console.log("Ready to rock");
 
 function component() {
     const element = document.createElement('div');
+    const element1 = document.createElement('div');
+    const element2 = document.createElement('div');
 
-    element.classList.add('outerContainer');
+    element.id ='outerContainer';
+    element1.id = 'navBar';
+    element2.id = 'mainSec';
+
+
+    element.appendChild(element1);
+    element.appendChild(element2);
+
 
     return element;
 
 }
 
 
-let outerCon = document.body.appendChild(component());
+document.body.appendChild(component());
