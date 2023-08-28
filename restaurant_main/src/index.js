1db7a8e4d7690acd07baa37422fb3e003d2f72f4
rsterenchak <rsterenchak@gmail.com>
2023-08-28 01:05:55 +0000

    navBar styling for grid

--- restaurant_main/src/index.js
+++ restaurant_main/src/index.js
@@ -6,15 +6,21 @@ console.log("Ready to rock");
 function component() {
     const element = document.createElement('div');
     const element1 = document.createElement('div');
+    const text1 = document.createElement('div');
     const element2 = document.createElement('div');
 
     element.id ='outerContainer';
     element1.id = 'navBar';
     element2.id = 'mainSec';
 
+    text1.id = 'heading';
+    text1.textContent = "My Restaurant Page";
+
 
     element.appendChild(element1);
     element.appendChild(element2);
+
+    element1.appendChild(text1);
 
 
     return element;
